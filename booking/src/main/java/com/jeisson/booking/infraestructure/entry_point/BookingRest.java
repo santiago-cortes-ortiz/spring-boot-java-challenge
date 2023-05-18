031c85452e1bc574b79ab4fd73ab19be07a36e02
jeiss <jeissoncortes02@outlook.com>
2023-05-18 07:33:34 -0500

    Crear servicios rest expuestos

---
+++ booking/src/main/java/com/jeisson/booking/infraestructure/entry_point/BookingRest.java
@@ -0,0 +1,49 @@
+package com.jeisson.booking.infraestructure.entry_point;
+
+import com.jeisson.booking.domain.model.Booking;
+import com.jeisson.booking.domain.model.common.ServiceException;
+import com.jeisson.booking.domain.usecase.BookingUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/booking")
+@RequiredArgsConstructor
+public class BookingRest {
+    private final BookingUseCase bookingUseCase;
+    @GetMapping("/{id}")
+    public ResponseEntity findById(String id){
+        try {
+            return new ResponseEntity<>(bookingUseCase.findById(id), HttpStatus.OK);
+        }catch (ServiceException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping
+    public ResponseEntity save(Booking booking){
+        try {
+            return new ResponseEntity<>(bookingUseCase.save(booking), HttpStatus.CREATED);
+        }catch (ServiceException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+    @PutMapping
+    public ResponseEntity update(Booking booking){
+        try {
+            return new ResponseEntity<>(bookingUseCase.update(booking), HttpStatus.OK);
+        }catch (ServiceException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(String id){
+        try {
+            bookingUseCase.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (ServiceException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+}
