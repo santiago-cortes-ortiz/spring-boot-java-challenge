4e9adb3519830bff6ef18991ba0048b73382c066
jeiss <jeissoncortes02@outlook.com>
2023-05-18 07:40:05 -0500

    Crear beans

--- booking/src/main/java/com/jeisson/booking/application/UseCaseConfig.java
+++ booking/src/main/java/com/jeisson/booking/application/UseCaseConfig.java
@@ -1,7 +1,21 @@
 package com.jeisson.booking.application;
 
+import com.jeisson.booking.domain.model.BookingRepositoryGateway;
+import com.jeisson.booking.domain.usecase.BookingUseCase;
+import com.jeisson.booking.infraestructure.driver_adapter.mapper_helper.BookingMapper;
+import com.jeisson.booking.infraestructure.driver_adapter.mongo_repository.BookingRepository;
+import com.jeisson.booking.infraestructure.driver_adapter.mongo_repository.BookingRepositoryDataAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UseCaseConfig {
+    @Bean
+    public BookingRepositoryDataAdapter bookingRepositoryDataAdapter(BookingRepository bookingRepository, BookingMapper bookingMapper){
+        return new BookingRepositoryDataAdapter(bookingRepository,bookingMapper);
+    }
+    @Bean
+    public BookingUseCase bookingUseCase(BookingRepositoryGateway bookingRepositoryGateway){
+        return new BookingUseCase(bookingRepositoryGateway);
+    }
 }
